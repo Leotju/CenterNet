4e6b0ec5eeeabdc071434f6b77d740971df0a36d
Leotju <xj65112338>
2019-05-13 18:09:42 +0800

    0513

--- src/lib/models/networks/resnet_dcn.py
+++ src/lib/models/networks/resnet_dcn.py
@@ -17,7 +17,7 @@ import torch
 import torch.nn as nn
 from .DCNv2.dcn_v2 import DCN
 import torch.utils.model_zoo as model_zoo
-
+from mmcv.cnn import vgg
 BN_MOMENTUM = 0.1
 logger = logging.getLogger(__name__)
 
