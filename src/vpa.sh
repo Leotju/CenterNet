1a6feb29f139193d2e1cfdcc6bf06886a9061c72
Leotju <xj65112338>
2019-05-17 10:58:06 +0800

    0513

--- src/vpa.sh
+++ src/vpa.sh
@@ -3,4 +3,5 @@ python test.py ctdet --arch fatnetfrnmbdasppdcn --dataset pascal --gpus 0,1 --do
 
 
 
-python main.py ctdet --arch fatnetfrndladasppdcnatt --dataset pascal --gpus 0,1 --down_ratio 1 --input_res 96 --num_epochs 210 --lr_step 135,180 --batch_size 2 --lr 1.25e-3 --exp_id fatnet_frn_pascal_96_daspp_dcn_dla_att_lr10x
+python main.py ctdet --arch fatnetfrndladasppdcnatt --dataset pascal --gpus 0,1 --down_ratio 1 --input_res 96 --num_epochs 210 --lr_step 135,180 --batch_size 32 --lr 1.25e-3 --exp_id fatnet_frn_pascal_96_daspp_dcn_dla_att_lr10x
+python test.py ctdet --arch fatnetfrndladasppdcnatt --dataset pascal --gpus 0,1 --down_ratio 1 --input_res 96 --num_epochs 210 --lr_step 135,180 --batch_size 32 --lr 1.25e-3 --exp_id fatnet_frn_pascal_96_daspp_dcn_dla_att_lr10x --resume
