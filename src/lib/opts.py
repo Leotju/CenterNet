78d944add45adaf6bfedf06d7348deca7bff19ff
Leotju <xj65112338>
2019-05-25 12:10:21 +0800

    0513

--- src/lib/opts.py
+++ src/lib/opts.py
@@ -44,7 +44,7 @@ class opts(object):
                              help='random seed') # from CornerNet
 
     # log
-    self.parser.add_argument('--print_iter', type=int, default=10,
+    self.parser.add_argument('--print_iter', type=int, default=0,
                              help='disable progress bar and print to screen.')
     self.parser.add_argument('--hide_data_time', action='store_true',
                              help='not display time during training.')
