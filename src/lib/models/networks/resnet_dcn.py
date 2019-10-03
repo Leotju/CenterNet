560d26c8a890cef375341880de448afe7fb408af
Leotju <jinxie@tju.edu.cn>
2019-10-03 08:03:07 +0800

    0809

--- src/lib/models/networks/resnet_dcn.py
+++ src/lib/models/networks/resnet_dcn.py
@@ -279,7 +279,8 @@ class PoseResNet(nn.Module):
 
         x = self.deconv_layers(x)
 
-        x = self.glo_re(x)
+        x = F.interpolate(x, scale_factor=2)
+        # x = self.glo_re(x)
         # x = self.glo_re_leo(x)
 
         ret = {}
