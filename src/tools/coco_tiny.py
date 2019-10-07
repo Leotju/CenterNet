2cd67ae6ef8570c7f7d36577a457f4ec848e7ef0
Leotju <xj65112338>
2019-10-07 21:19:07 +0800

    0513

---
+++ src/tools/coco_tiny.py
@@ -0,0 +1,18 @@
+from pycocotools.coco import COCO
+import json, os
+
+coco_root = './data/coco/annotations'
+ann = COCO(os.path.join(coco_root, 'instances_train2017.json'))
+ann_js = json.load(open(os.path.join(coco_root, 'instances_train2017.json')))
+filter_boxes = []
+filter_imgs = []
+for img_id, img in enumerate(ann.imgs):
+    if img_id < 5000:
+        filter_imgs.append(ann.imgs[img])
+        boxes = ann.imgToAnns[img_id]
+        for box in boxes:
+            filter_boxes.append(box)
+ann_js['images'] = filter_imgs
+ann_js['annotations'] = filter_boxes
+
+json.dump(ann_js, open(os.path.join(coco_root, 'instances_tiny_train2017.json')))
