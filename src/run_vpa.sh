fbd5dfa2bc6351a704f89ce086c26c3dac31f837
Leotju <xj65112338>
2019-10-07 22:38:22 +0800

    0513

--- src/run_vpa.sh
+++ src/run_vpa.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
-python main.py ctdet --exp_id coco_resdcn18 --arch resdcn_18 --batch_size 64 --master_batch 18 --lr 2.5e-4 --gpus 0,1 --num_workers 8 --dataset coco_tiny
+sleep 4h
+python main.py ctdet --exp_id coco_vggdcn18 --arch vgg_16 --gpus 0,1 --num_workers 8 --dataset coco_tiny --batch_size 16 --lr 0.625e-4
